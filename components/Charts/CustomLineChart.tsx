bbc202aeb5de70248f9d72865854d206d04bdf1b
Joselo <jl.garciareymundo@gmail.com>
2023-02-27 20:01:41 -0600

    custom label

--- components/Charts/CustomLineChart.tsx
+++ components/Charts/CustomLineChart.tsx
@@ -9,6 +9,7 @@ import {
   Legend
 } from 'chart.js'
 import { Line } from 'react-chartjs-2'
+import currencyFormat from '@/lib/utils/format'
 
 ChartJS.register(
   CategoryScale,
@@ -29,10 +30,39 @@ export const options = {
     title: {
       display: false,
     },
+    tooltip: {
+      callbacks: {
+        label: function(context: { dataset: { label: string; }; parsed: { y: number | null | undefined; }; }) {
+          let label = context.dataset.label || '';
+          if(label) {label += ': '}
+          if (context.parsed.y !== null) {
+            label += currencyFormat(context.parsed.y)
+          }
+          return label
+        },
+      }
+    }
+  },
+  scales: {
+    y: {
+      display: false,
+    },
+    x: {
+      ticks: {
+        font: {
+          size: 8
+        },
+        maxRotation: 0,
+        
+      },
+      grid: {
+        color: 'rgba(216, 224, 220, 0.2)',
+      }
+    }
   },
 };
 
-const labels = ['01:00 am', '02:00 am', '03:00 am', '04:00 am', '05:00 am', '06:00 am', '07:00 am', '08:00 am', '09:00 am', '10:00 am', '11:00 am', '12:00 pm', '01:00 pm', '02:00 pm', '03:00 pm', '04:00 pm', '05:00 pm', '06:00 pm', '07:00 pm', '08:00 pm', '09:00 pm', '10:00 pm', '11:00 pm', '12:00 pm']
+const labels = ['01:00 am', '02:00 am', '03:00 am', '04:00 am', '05:00 am', '06:00 am', '07:00 am', '08:00 am', '09:00 am', '10:00 am', '11:00 am', '12:00 pm', '01:00 pm', '02:00 pm', '03:00 pm', '04:00 pm', '05:00 pm', '06:00 pm', '07:00 pm', '08:00 pm', '09:00 pm', '10:00 pm', '11:00 pm', '12:00 am']
 
 export default function CustomLineChart(props: {data: number[] | undefined}) {
   const data = {
@@ -41,8 +71,10 @@ export default function CustomLineChart(props: {data: number[] | undefined}) {
       {
         label: 'Ingresos por hora',
         data: props.data,
-        borderColor: 'rgb(41, 229, 90)',
-        backgroundColor: 'rgba(41, 229, 90, 0.5)'
+        borderColor: 'rgb(41, 229, 146)',
+        backgroundColor: 'rgba(41, 229, 146, 0.5)',
+        borderWidth: 1,
+        radius: 2
       }
     ]
   }
